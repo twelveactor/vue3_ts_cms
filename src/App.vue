170f12f159a4169310b1375b334a4dfee38d41db
“twelveactor <2918747479@qq.com>
2022-04-25 12:27:20 +0800

    style(init): init project
    n

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: "App",
+  components: {}
+});
+</script>
+
+<style lang="less">
+
+</style>
